54837b5cc94ba35a033e6e91d7a07cca74b68401
snehanagalingaiah <snehanagalingaiah@gmail.com>
2023-04-08 12:25:00 +0530

    Add files via upload

---
+++ Querysheet.js
@@ -0,0 +1,1422 @@
+use "day_38_task"
+
+//db.createCollection("users");
+//db.createCollection("topics");
+//db.createCollection("tasks");
+//db.createCollection("mentors");
+//db.createCollection("codekata");
+//db.createCollection("company_drives");
+
+
+
+
+
+
+/*db.topics.insertMany([
+{
+	"topic_id" : 1,
+	"topic_name" : "Html-1"
+},
+
+{
+
+	"topic_id" : 2,
+	"topic_name" : "Html-2"
+},
+
+
+{
+	"topic_id" : 3,
+	"topic_name" : "Css-1"
+},
+
+
+{
+	"topic_id" : 4,
+	"topic_name" : "Css-2"
+},
+
+
+{
+	"topic_id" : 5,
+	"topic_name" : "JS-1"
+},
+
+
+{
+	"topic_id" : 6,
+	"topic_name" : "JS-2"
+},
+
+
+{
+	"topic_id" : 7,
+	"topic_name" : "Reactjs-1"
+},
+
+{
+	"topic_id" : 8,
+	"topic_name" : "Reactjs-2"
+},
+
+{
+
+	"topic_id" : 9,
+	"topic_name" : "Sql-1"
+},
+
+
+{
+
+	"topic_id" : 10,
+	"topic_name" : "Sql-2"
+},
+
+
+{
+
+	"topic_id" : 11,
+	"topic_name" : "Redux-1"
+},
+
+
+{
+	"topic_id" : 12,
+	"topic_name" : "Nodejs-1"
+},
+
+
+{
+	"topic_id" : 13,
+	"topic_name" : "Nodejs-2"
+}
+    ])*/
+    
+    
+    
+    
+    
+    
+    
+    /*db.tasks.insertMany([
+{
+	"task_id" : 1,
+	"task_name" : "Html-1"
+},
+
+{
+	"task_id" : 2,
+	"task_name" : "Html-2"
+},
+
+{
+	"task_id" : 3,
+	"task_name" : "Css-1"
+},
+
+{
+	"task_id" : 4,
+	"task_name" : "Css-2"
+},
+
+{
+
+	"task_id" : 5,
+	"task_name" : "JS-1"
+},
+
+{
+
+	"task_id" : 6,
+	"task_name" : "JS-2"
+},
+
+{
+	"task_id" : 7,
+	"task_name" : "Reactjs-1"
+},
+
+{
+	"task_id" : 8,
+	"task_name" : "Reactjs-2"
+},
+
+{
+	"task_id" : 9,
+	"task_name" : "Sql-1"
+},
+
+{
+	"task_id" : 10,
+	"task_name" : "Sql-2"
+},
+
+{
+	"task_id" : 11,
+	"task_name" : "Redux-1"
+},
+
+{
+	"task_id" : 12,
+	"task_name" : "Nodejs-1"
+},
+
+{
+	"task_id" : 13,
+	"task_name" : "Nodejs-2"
+}
+    ])*/
+    
+    
+    
+    
+    
+    
+    
+    
+ /*   db.users.insertMany([
+{
+	"user_id" : 1,
+	"name" : "Anvit",
+	"mentor_id" : 3
+},
+
+{
+	"user_id" : 2,
+	"name" : "Beena",
+	"mentor_id" : 2
+},
+
+{
+	"user_id" : 3,
+	"name" : "Chetan",
+	"mentor_id" : 5
+},
+
+{
+	"user_id" : 4,
+	"name" : "Divya",
+	"mentor_id" : 1
+},
+
+{
+	"user_id" : 5,
+	"name" : "Esther",
+	"mentor_id" : 4
+},
+
+{
+	"user_id" : 6,
+	"name" : "Farrukh",
+	"mentor_id" : 2
+},
+
+{
+	"user_id" : 7,
+	"name" : "Grishma",
+	"mentor_id" : 4
+},
+
+{
+	"user_id" : 8,
+	"name" : "Hari",
+	"mentor_id" : 2
+},
+
+{
+	"user_id" : 9,
+	"name" : "Isha",
+	"mentor_id" : 5
+},
+
+{
+	"user_id" : 10,
+	"name" : "Jeevan",
+	"mentor_id" : 3
+},
+
+{
+	"user_id" : 11,
+	"name" : "Kelvin",
+	"mentor_id" : 3
+},
+
+{
+	"user_id" : 12,
+	"name" : "Lisa",
+	"mentor_id" : 2
+},
+
+{
+	"user_id" : 13,
+	"name" : "Mayuri",
+	"mentor_id" : 2
+},
+
+{
+	"user_id" : 14,
+	"name" : "Naksh",
+	"mentor_id" : 5
+},
+
+{
+	"user_id" : 15,
+	"name" : "Preeti",
+	"mentor_id" : 5
+},
+
+{
+	"user_id" : 16,
+	"name" : "Sneha",
+	"mentor_id" : 1
+}
+    
+    ])*/
+    
+    
+    
+    
+    
+    
+    
+    /*db.mentors.insertMany([
+{
+	"mentor_id" : 1,
+	"mentor_name" : "Abhishek"
+},
+
+{
+	"mentor_id" : 2,
+	"mentor_name" : "Asha"
+},
+
+{
+	"mentor_id" : 3,
+	"mentor_name" : "Chaitra"
+},
+
+{
+	"mentor_id" : 4,
+	"mentor_name" : "Gagan"
+},
+
+{
+	"mentor_id" : 5,
+	"mentor_name" : "Suman"
+}
+    
+    ])*/
+    
+    
+    
+    
+    
+    
+
+/*db.codekata.insertMany([
+{
+	"question_id" : 1,
+	"completed_user_id" : 2
+},
+{
+	"question_id" : 1,
+	"completed_user_id" : 5
+},
+{
+	"question_id" : 1,
+	"completed_user_id" : 7
+},
+{
+	"question_id" : 1,
+	"completed_user_id" : 11
+},
+{
+	"question_id" : 1,
+	"completed_user_id" : 16
+},
+
+{
+	"question_id" : 2,
+	"completed_user_id" : 3
+},
+{
+	"question_id" : 2,
+	"completed_user_id" : 4
+},
+{
+	"question_id" : 2,
+	"completed_user_id" : 5
+},
+{
+	"question_id" : 2,
+	"completed_user_id" : 14
+},
+{
+	"question_id" : 2,
+	"completed_user_id" : 16
+},
+
+{
+	"question_id" : 3,
+	"completed_user_id" : 11
+},
+{
+	"question_id" : 3,
+	"completed_user_id" : 12
+},
+{
+	"question_id" : 3,
+	"completed_user_id" : 13
+},
+{
+	"question_id" : 3,
+	"completed_user_id" : 14
+},
+{
+	"question_id" : 3,
+	"completed_user_id" : 16
+},
+
+{
+	"question_id" : 4,
+	"completed_user_id" : 1
+},
+{
+	"question_id" : 4,
+	"completed_user_id" : 4
+},
+{
+	"question_id" : 4,
+	"completed_user_id" : 5
+},
+{
+	"question_id" : 4,
+	"completed_user_id" : 16
+},
+{
+	"question_id" : 4,
+	"completed_user_id" : 11
+},
+
+{
+	"question_id" : 5,
+	"completed_user_id" : 3
+},
+{
+	"question_id" : 5,
+	"completed_user_id" : 9
+},
+{
+	"question_id" : 5,
+	"completed_user_id" : 12
+},
+{
+	"question_id" : 5,
+	"completed_user_id" : 13
+},
+{
+	"question_id" : 5,
+	"completed_user_id" : 16
+},
+
+{
+	"question_id" : 6,
+	"completed_user_id" : 1
+},
+{
+	"question_id" : 5,
+	"completed_user_id" : 4
+},
+{
+	"question_id" : 5,
+	"completed_user_id" : 14
+},
+{
+	"question_id" : 5,
+	"completed_user_id" : 15
+},
+{
+	"question_id" : 5,
+	"completed_user_id" : 16
+},
+
+{
+	"question_id" : 7,
+	"completed_user_id" : 7
+},
+{
+	"question_id" : 7,
+	"completed_user_id" : 8
+},
+{
+	"question_id" : 7,
+	"completed_user_id" : 9
+},
+{
+	"question_id" : 7,
+	"completed_user_id" : 10
+},
+{
+	"question_id" : 7,
+	"completed_user_id" : 16
+},
+{
+	"question_id" : 8,
+	"completed_user_id" : 4
+},
+{
+	"question_id" : 8,
+	"completed_user_id" : 5
+},
+{
+	"question_id" : 8,
+	"completed_user_id" : 2
+},
+{
+	"question_id" : 8,
+	"completed_user_id" : 11
+},
+{
+	"question_id" : 8,
+	"completed_user_id" : 16
+},
+
+{
+	"question_id" : 9,
+	"completed_user_id" : 1
+},
+{
+	"question_id" : 9,
+	"completed_user_id" : 5
+},
+{
+	"question_id" : 9,
+	"completed_user_id" : 16
+},
+{
+	"question_id" : 9,
+	"completed_user_id" : 15
+},
+{
+	"question_id" : 9,
+	"completed_user_id" : 14
+},
+
+{
+	"question_id" : 10,
+	"completed_user_id" : 3
+},
+{
+	"question_id" : 10,
+	"completed_user_id" : 11
+},
+{
+	"question_id" : 10,
+	"completed_user_id" : 12
+},
+{
+	"question_id" : 10,
+	"completed_user_id" : 13
+},
+{
+	"question_id" : 10,
+	"completed_user_id" : 16
+},
+])*/
+
+
+
+
+
+
+/*db.attendance.insertMany([
+
+{
+	"class_id" : 1,
+	"topic_id" : 1,
+	"task_id" : 1,
+	"scheduled_date" : ISODate("2020-10-15T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		6,
+		7,
+		8,
+		9,
+		10,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		6,
+		7,
+		8,
+		9,
+		10,
+		12,
+		13,
+		14,
+		15,
+		16
+	]
+},
+
+{
+	"class_id" : 2,
+	"topic_id" : 2,
+	"task_id" : 2,
+	"scheduled_date" : ISODate("2020-10-16T05:30:00.000+05:30"),
+	"present_user_id" : [
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		15,
+		16
+	]
+},
+
+{
+	"class_id" : 3,
+	"topic_id" : 3,
+	"task_id" : 3,
+	"scheduled_date" : ISODate("2020-10-17T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		13,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		13,
+		15,
+		16
+	]
+},
+
+{
+	"class_id" : 4,
+	"topic_id" : 4,
+	"task_id" : 4,
+	"scheduled_date" : ISODate("2020-10-18T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		8,
+		9,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		2,
+		3,
+		4,
+		5,
+		6,
+		8,
+		9,
+		11,
+		12,
+		13,
+		15,
+		16
+	]
+},
+
+{
+	"class_id" : 5,
+	"topic_id" : 5,
+	"task_id" : 5,
+	"scheduled_date" : ISODate("2020-10-19T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	]
+},
+
+{
+	"class_id" : 6,
+	"topic_id" : 6,
+	"task_id" : 6,
+	"scheduled_date" : ISODate("2020-10-20T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		4,
+		5,
+		6,
+		7,
+		8,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		4,
+		5,
+		6,
+		7,
+		8,
+		10,
+		13,
+		14,
+		15,
+		16
+	]
+},
+
+{
+	"class_id" : 7,
+	"topic_id" : 7,
+	"task_id" : 7,
+	"scheduled_date" : ISODate("2020-10-21T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15
+	]
+},
+
+{
+	"class_id" : 8,
+	"topic_id" : 8,
+	"task_id" : 8,
+	"scheduled_date" : ISODate("2020-10-22T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		16
+	]
+},
+
+{
+	"class_id" : 9,
+	"topic_id" : 9,
+	"task_id" : 9,
+	"scheduled_date" : ISODate("2020-10-23T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		9,
+		10,
+		11,
+		12,
+		14,
+		15,
+		16
+	]
+},
+
+
+{
+
+	"class_id" : 10,
+	"topic_id" : 10,
+	"task_id" : 10,
+	"scheduled_date" : ISODate("2020-10-24T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	]
+},
+
+{
+	"class_id" : 11,
+	"topic_id" : 11,
+	"task_id" : 11,
+	"scheduled_date" : ISODate("2020-10-25T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		8,
+		9,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		3,
+		4,
+		5,
+		6,
+		8,
+		9,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	]
+},
+
+{
+	"class_id" : 12,
+	"topic_id" : 12,
+	"task_id" : 12,
+	"scheduled_date" : ISODate("2020-10-26T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		8,
+		9,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		8,
+		9,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	]
+},
+
+{
+	"class_id" : 13,
+	"topic_id" : 13,
+	"task_id" : 13,
+	"scheduled_date" : ISODate("2020-10-27T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		16
+	]
+},
+
+{
+	"class_id" : 14,
+	"topic_id" : 14,
+	"task_id" : 14,
+	"scheduled_date" : ISODate("2020-10-28T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		15,
+		16
+	]
+},
+
+{
+	"class_id" : 15,
+	"topic_id" : 15,
+	"task_id" : 15,
+	"scheduled_date" : ISODate("2020-10-29T05:30:00.000+05:30"),
+	"present_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	],
+	"task_submitted_user_id" : [
+		1,
+		2,
+		3,
+		4,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	]
+}
+
+    ]) */
+
+
+
+
+
+
+/*db.company_drives.insertMany([
+{
+
+	"drive_id" : 1,
+	"company_name" : "Infosys",
+	"drive_date" : ISODate("2020-10-12T05:30:00.000+05:30"),
+	"user_id_attended" : [
+		2,
+		3,
+		5,
+		6,
+		7,
+		8,
+		9,
+		11,
+		13,
+		15
+	]
+},
+
+{
+	"drive_id" : 2,
+	"company_name" : "Wipro",
+	"drive_date" : ISODate("2020-10-16T05:30:00.000+05:30"),
+	"user_id" : [
+		1,
+		3,
+		5,
+		7,
+		9,
+		11,
+		13,
+		15
+	]
+},
+
+{
+	"drive_id" : 3,
+	"company_name" : "Accenture",
+	"drive_date" : ISODate("2020-10-22T05:30:00.000+05:30"),
+	"user_id" : [ 2, 4, 6, 8, 10 ]
+},
+
+
+{
+	"drive_id" : 4,
+	"company_name" : "TCS",
+	"drive_date" : ISODate("2020-10-30T05:30:00.000+05:30"),
+	"user_id" : [
+		3,
+		9,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16
+	]
+},
+
+{
+	"drive_id" : 5,
+	"company_name" : "MindTree",
+	"drive_date" : ISODate("2020-11-07T05:30:00.000+05:30"),
+	"user_id" : [ 1, 7, 10 ]
+},
+
+{
+	"drive_id" : 6,
+	"company_name" : "HappiestMinds",
+	"drive_date" : ISODate("2020-11-16T05:30:00.000+05:30"),
+	"user_id" : [
+		2,
+		4,
+		5,
+		6,
+		7,
+		11,
+		12,
+		14,
+		16
+	]
+},
+
+{
+	"drive_id" : 7,
+	"company_name" : "Verisk Financials",
+	"drive_date" : ISODate("2020-11-28T05:30:00.000+05:30"),
+	"user_id" : [
+		1,
+		2,
+		4,
+		6,
+		8,
+		10
+	]
+},
+
+
+{
+	"drive_id" : 8,
+	"company_name" : "Cognizant",
+	"drive_date" : ISODate("2020-12-05T05:30:00.000+05:30"),
+	"user_id" : [ 12, 13, 14, 15, 16 ]
+},
+
+
+{
+	"drive_id" : 9,
+	"company_name" : "Capgemini",
+	"drive_date" : ISODate("2020-12-13T05:30:00.000+05:30"),
+	"user_id" : [
+		2,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10
+	]
+},
+
+{
+	"drive_id" : 10,
+	"company_name" : "Honeywell",
+	"drive_date" : ISODate("2020-12-23T05:30:00.000+05:30"),
+	"user_id" : [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6
+	]
+}
+    ])*/
+
+
+
+
+
+
+
+
+
+//1.Find all the topics and tasks which are thought in the month of October
+
+/*db.attendance.aggregate([
+    {
+        $lookup:{
+       from: "topics",
+       localField: "topic_id",
+       foreignField: "topic_id",
+       as: "topics_data"
+        }
+    },
+    {
+        $lookup:{
+       from: "tasks",
+       localField: "task_id",
+       foreignField: "task_id",
+       as: "tasks_data"
+        }
+    },
+    {
+        $match: {
+            scheduled_date:{$gte:new Date("2020-10-01"),$lte:new Date("2020-10-31")}
+        }
+    },
+    {
+        $project: {
+            class_id:1,
+            "topics_data.topic_name":1,
+            "tasks_data.task_name":1,
+            scheduled_date:1
+        }
+    }
+    ])*/
+    
+    //2. Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
+    
+   // db.company_drives.find({drive_date:{$gte:new Date("2020-10-15"),$lte:new Date("2020-10-31")}})
+   
+   //3. Find all the company drives and students who are appeared for the placement.
+   
+   /*db.company_drives.aggregate([
+       {
+           $lookup:{
+            from: "users",
+            localField: "user_id_attended",
+            foreignField: "user_id",
+            as: "users_who_attended"
+           }
+       }
+       ])*/
+       
+       //4. Find the number of problems solved by the user in codekata
+       
+
+ /*      db.codekata.aggregate([
+           {
+               $group: { 
+               _id: "$completed_user_id",
+               noOfQuestions: {$sum:1}
+               }
+           }
+           ])*/
+           
+           
+           //5. Find all the mentors with who has the mentee's count more than equal to or more than 3 (changed from 15 to 3)
+           
+           /*db.users.aggregate([
+           {
+               $group: { 
+               _id: "$mentor_id",
+               noofmentees: {$sum:1}
+               }
+           },
+           {
+               $match: {
+                   noofmentees: {$gte:3}
+               }
+           },
+            {  
+                $lookup: {
+                     from: "mentors",
+                     localField: "_id",
+                     foreignField: "mentor_id",
+                     as: "mentor_data"
+                   }
+            }
+           ])*/
+           
+           
+           //6.Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+           
+           /*db.attendance.aggregate([
+           {
+               $match:{
+                   scheduled_date:{$gte:new Date("2020-10-15"),$lte:new Date("2020-10-31")}
+               }
+           },
+           
+           {
+                 "$project": {
+                     class_id:1,
+                     scheduled_date:1,
+                     "absentees_from_class": {
+                           $let:
+                              {
+                                vars: { "all_students": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16] },
+                                in: {  "$setDifference": ["$$all_students","$present_user_id" }
+                              }
+                    
+                }
+                     "tasks_not_submitted_students": {
+                           $let:
+                              {
+                                vars: { "all_students": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16] },
+                                in: {  "$setDifference": ["$$all_students","$task_submitted_user_id" }
+                              }
+                    
+                }                
+            }
+         },
+         {
+                 $lookup: {
+                        from: "users",
+                        localField: "absentees_from_class",
+                        foreignField: "user_id",
+                        as: "absentee_details"
+                      }
+         },
+         {
+                  $lookup: {
+                        from: "users",
+                        localField: "tasks_not_submitted_students",
+                        foreignField: "user_id",
+                        as: "task_not_submitted_student_details"
+                      }
+         }
+])*/
+       
